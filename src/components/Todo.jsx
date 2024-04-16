cd54ec4c21911a0bad17935abc1eb30ccf70f275
heyvipul <vipulgirhestar@gmail.com>
2024-04-16 11:19:29 +0530

    editFunction-bug-fix

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,11 +9,14 @@ const Todo = () => {
 
   const [data, setData] = useState([]);
   const [editIndex, setEditIndex] = useState(null);
+  const [name, setName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [department, setDepartment] = useState("")
 
   const getRequest = async () => {
     try {
       const { data } = await axios.get(getUsers())
-      // console.log(data);
       setData(data)
     } catch (error) {
       console.log(error);
@@ -29,10 +32,21 @@ const Todo = () => {
   };
 
   const handleUpdate = async (index) => {
-    setEditIndex(null)
-  };
+    const combinedName = name + ' ' + lastName; 
+    try {
+      const updatedData = [...data];
+      updatedData[index] = { ...updatedData[index], name: combinedName, email, company: { name: department } };
+      await axios.put(`https://jsonplaceholder.typicode.com/users/${index+1}`, updatedData[index]);
+      setData(updatedData);
+      setEditIndex(null);
+      alert("Data Updated Successfully!")
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  console.log(data);
+
+
 
   return (
     <>
@@ -51,12 +65,11 @@ const Todo = () => {
         {data?.map(function (ele, index) {
           return <tr key={index}>
             <td>{ele.id}</td>
-            <td>{editIndex === index ? <input type="text" defaultValue={ele.name.split(' ')[0]} /> : `${ele.name.split(' ')[0]}`}</td>
-            {/* <td>{ele.name.split(' ')[0]}</td> */}
-            <td>{editIndex === index ? <input type="text" defaultValue={ele.name.split(' ')[1]} /> : `${ele.name.split(' ')[1]}`}</td>
-            <td>{editIndex === index ? <input type="text" defaultValue={ele.email} /> : `${ele.email}`}</td>
-            <td>{editIndex === index ? <input type="text" defaultValue={ele.company.name} /> : `${ele.company.name}`}</td>
-            <td>
+            <td>{editIndex === index ? <input onChange={(e) => setName(e.target.value)} type="text" defaultValue={ele.name.split(' ')[0]} /> : `${ele.name.split(' ')[0]}`}</td>
+            <td>{editIndex === index ? <input onChange={(e) => setLastName(e.target.value)} type="text" defaultValue={ele.name.split(' ')[1]} /> : `${ele.name.split(' ')[1]}`}</td>
+            <td>{editIndex === index ? <input onChange={(e) => setEmail(e.target.value)} type="email" defaultValue={ele.email} /> : `${ele.email}`}</td>
+            <td>{editIndex === index ? <input onChange={(e) => setDepartment(e.target.value)} type="text" defaultValue={ele.company.name} /> : `${ele.company.name}`}</td>
+            <td className="actions-btn">
               {editIndex === index ? (
                 <button onClick={() => handleUpdate(index)}>Update <span><MdOutlineBrowserUpdated /></span></button>
               ) : (
